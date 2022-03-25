b03f0d1493b04d6a63d60e8f9579eb139e301418
jaemjeon <jaemjeon@42seoul.kr>
2022-03-25 16:13:37 +0900

    3/25

---
+++ gnl_array/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaemjeon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/18 19:51:49 by jaemjeon          #+#    #+#             */
+/*   Updated: 2022/03/25 15:46:09 by jaemjeon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+int		ft_strchr(char *str, char to_find);
+void	*ft_memcpy(void *dst, void *src, size_t n);
+char	*ft_strjoin(char **p_s1, char *s2);
+int		in_newline(char *str);
+
+#endif
